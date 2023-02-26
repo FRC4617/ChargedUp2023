849cb5bf821c8f40ccefe5d33bd12a0ea20e8bb9
FionaAi <94726692+FionaAi@users.noreply.github.com>
2023-02-26 14:45:25 -0500

    Added Auto Options

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,37 +5,62 @@
 package frc.robot;
 
 import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.DriveTime;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Drivetrain k_drivetrain = Drivetrain.getInstance();
 
+  private final Command basic = new DriveTime(k_drivetrain, 0.5, 0).withTimeout(1.5);
+  private final Command advancedMiddle = new SequentialCommandGroup(
+      new DriveTime(k_drivetrain, 0.5, 0).withTimeout(1.5), // exit community
+      new DriveTime(k_drivetrain, 0.5, 0).withTimeout(1.5) // drive onto docking station
+  );
+  private final Command advancedRight = new SequentialCommandGroup(
+      new DriveTime(k_drivetrain, 0.5, 0).withTimeout(1.5), // exit community
+      new DriveTime(k_drivetrain, 0.5, 0).withTimeout(1.5) // drive onto docking station
+  );
+  private final Command advancedLeft = new SequentialCommandGroup(
+      new DriveTime(k_drivetrain, 0.5, 0).withTimeout(1.5), // exit community
+      new DriveTime(k_drivetrain, 0.5, 0).withTimeout(1.5) // drive onto docking station
+  );
+
+  // A chooser for autonomous commands
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
+
   public static XboxController k_driver = new XboxController(0);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     k_drivetrain.setDefaultCommand(new ArcadeDrive(k_drivetrain));
 
-  }
+    // Add commands to the autonomous command chooser
+    m_chooser.setDefaultOption("Simple Auto", basic);
+    m_chooser.addOption("Middle Auto", advancedMiddle);
+    m_chooser.addOption("Right Auto", advancedRight);
+    m_chooser.addOption("Left Auto", advancedLeft);
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
+    // Put the chooser on the dashboard
+    SmartDashboard.putData(m_chooser);
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -43,12 +68,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-   
-
-
-
-
-
-    return null;
+    return m_chooser.getSelected();
   }
 }
